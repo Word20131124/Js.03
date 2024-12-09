0159c6b3c3d21e40bb234f59c5d36d17552f36f9
Word20131124 <miron.mir2913@gmail.com>
2024-12-09 20:50:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+alert ('Sizda hozir random sonlar chiqishni boshledi');
+
+alert ('Random orqali chiqqan son: 22');
+
+confirm ('biz random sonni 2 ga kopaytirmoqchi va bolmoqchimiz !');
+
+alert (`Random orqali chiqqan son: 22 edi
+biz uni 2 ga kopaytirib 44 sonini chiqardik,
+va shu sonni 2 ga bolib: 11 sonni ciqardik,
+va shu sonni 3 ga bolgandagi qoldiq: 1 sonini chiqardik
+`);
+
+    alert ('Sizda hozir random sonlar chiqishni boshledi');
+
+    alert ('Random orqali chiqqan son: 55');
+
+    confirm ('biz random sonni 2 ga kopaytirmoqchi va bolmoqchimiz !');
+
+    alert (`Random orqali chiqqan son: 55 edi
+biz uni 2 ga kopaytirib 110 sonini chiqardik,
+va shu sonni 2 ga bolib: 27.5 sonni ciqardik,
+va shu sonni 3 ga bolgandagi qoldiq: 1 sonini chiqardik
+`);
